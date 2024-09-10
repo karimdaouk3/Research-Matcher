37fe8321bf129671375673bca192e727ea0f9425
karimdaouk3 <106763257+karimdaouk3@users.noreply.github.com>
2024-09-09 22:17:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from openai import OpenAI
 import logging
 from datetime import datetime
 import os
-# from waitress import serve
+from dotenv import load_dotenv
+from waitress import serve
 
+load_dotenv()
 client = OpenAI(
-    api_key=os.getenv('OPENAI_API_KEY')
-)
+        api_key = os.getenv('API_KEY')
+        )
 
 app = Flask(__name__)
 
@@ -24,7 +26,7 @@ df = pd.DataFrame(loaded_data_list)
 # Initialize the sentence embedder model
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
 
-logging.basicConfig(filename='app_log.json', level=logging.INFO)
+logging.basicConfig(filename='app_log.txt', level=logging.INFO)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -40,32 +42,41 @@ def index():
         generate_title_checkbox = 'generateTitle' in request.form
         generate_title_checkbox_checked = generate_title_checkbox
 
+        # Assuming 'proposal_embedding' contains the 1D array of the embedded proposal vector
         proposal_embedding = model.encode([proposal])[0]
 
         # Calculate dot product for each row
         df['Dot Product'] = df['Research Embedding'].apply(lambda x: np.dot(proposal_embedding, x) if x is not None else None)
 
+        # Sort DataFrame based on dot product scores in descending order
         df_sorted = df.sort_values(by='Dot Product', ascending=False)
 
+        # Select top N rows based on the result count
         top_n_rows = df_sorted.head(result_count)
 
+        # Extract relevant information for rendering
         top_n_rows.loc[:, 'Name'] = top_n_rows['Name'].apply(lambda x: x.title() if x else x)
         top_n_rows.loc[:, 'Title'] = top_n_rows['Title'].apply(lambda x: x.title() if x else x)
 
+        # Convert to dictionary
         results = top_n_rows[['Name', 'Title', 'Research Summary']].to_dict(orient='records')
 
         generated_title = None
 
         if generate_title_checkbox:
+            # Generate a title using OpenAI chat-based language model
             generated_title = generate_title(proposal)
 
+        # Render the template
         return render_template('index.html', proposal=proposal, results=results, resultCount=result_count, generatedTitle=generated_title, generateTitleCheckbox=generate_title_checkbox_checked)
 
+    # For GET requests, set the checkbox to unchecked by default
     return render_template('index.html', generateTitleCheckbox=generate_title_checkbox_checked)
 
 def generate_title(proposal):
+    # Call the OpenAI API to generate a title using chat-based language model
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": "You are an assistant that generates a simple title for a research paper given a research proposal. Do not include the word title in the response."},
             {"role": "user", "content": proposal}
@@ -77,5 +88,5 @@ def generate_title(proposal):
     return response.choices[0].message.content.strip()
 
 if __name__ == '__main__':
-    app.run(port=7070, debug=False)
-    # serve(app, host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0')
